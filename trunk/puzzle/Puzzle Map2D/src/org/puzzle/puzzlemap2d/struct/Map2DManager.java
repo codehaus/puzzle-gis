773e0e6a24619225229a6056ab83f2f6d893574f
eclesia <eclesia@80afd94b-f245-0410-a355-d48bdef141ff>
2008-02-15 17:25:59 +0000

    Uploading Puzzle Map2D module.

---
+++ trunk/puzzle/Puzzle Map2D/src/org/puzzle/puzzlemap2d/struct/Map2DManager.java
@@ -0,0 +1,92 @@
+/*
+ *  Puzzle-GIS - OpenSource mapping program
+ *  http://docs.codehaus.org/display/PUZZLEGIS
+ *  Copyright (C) 2007 Puzzle-GIS
+ *  
+ *  GPLv3 + Classpath exception
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.puzzle.puzzlemap2d.struct;
+
+import java.awt.Color;
+import org.geotools.gui.swing.contexttree.JContextTree;
+import org.geotools.gui.swing.contexttree.column.SelectionTreeTableColumn;
+import org.geotools.gui.swing.contexttree.popup.LayerZoomItem;
+import org.geotools.gui.swing.map.map2d.JDefaultEditableMap2D;
+import org.geotools.gui.swing.map.map2d.control.JMap2DInfoBar;
+import org.geotools.gui.swing.map.map2d.strategy.MergeBufferedImageStrategy;
+import org.geotools.map.MapContext;
+import org.puzzle.puzzlecontexttree.struct.ContextTreeManager;
+import org.puzzle.puzzlecore.struct.Application;
+
+/**
+ *
+ * @author Johann Sorel
+ */
+public final class Map2DManager {
+
+    private static Map2DManager instance;
+    private JDefaultEditableMap2D map2d = null;
+    private JMap2DInfoBar infoBar = null;
+
+    private Map2DManager() {
+
+        JContextTree tree = ContextTreeManager.getInstance().getContextTree();
+
+        SelectionTreeTableColumn selectionColumn = new SelectionTreeTableColumn(getMap2D());
+        LayerZoomItem zoomLayerItem = new LayerZoomItem(getMap2D());
+
+        selectionColumn.setMap(getMap2D());
+        zoomLayerItem.setMap(getMap2D());
+
+        tree.addColumn(selectionColumn);
+        tree.getPopupMenu().addItem(0, zoomLayerItem);
+
+    }
+
+    public JDefaultEditableMap2D getMap2D() {
+
+        if (map2d == null) {
+            map2d = new JDefaultEditableMap2D();
+            map2d.setRenderingStrategy(new MergeBufferedImageStrategy());
+            map2d.setBackground(Color.WHITE);
+            map2d.setOpaque(true);
+        }
+
+        MapContext context = Application.getInstance().getActiveContext();
+        map2d.getRenderingStrategy().setContext(context);
+
+        return map2d;
+    }
+
+    public JMap2DInfoBar getMapInfoBar() {
+
+        if (infoBar == null) {
+            infoBar = new JMap2DInfoBar();
+            infoBar.setFloatable(false);            
+            infoBar.setMap(getMap2D());
+        }
+        
+
+        return infoBar;
+    }
+
+    public static Map2DManager getInstance() {
+        if (instance == null) {
+            instance = new Map2DManager();
+        }
+        return instance;
+    }
+}
