538e0ee2ffa89233a57eebc7f54c6681b32578af
eclesia <eclesia@80afd94b-f245-0410-a355-d48bdef141ff>
2008-06-21 12:28:45 +0000

    Adding project support and GIS project factory.

---
+++ puzzle/Puzzle Core/src/org/puzzle/puzzlecore/project/nodes/GISMapNode.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.puzzle.puzzlecore.project.nodes;
+
+import java.awt.Image;
+import org.openide.loaders.DataObjectNotFoundException;
+import org.openide.nodes.FilterNode;
+import org.openide.nodes.Node;
+import org.openide.util.Lookup;
+import org.openide.util.Utilities;
+import org.openide.util.lookup.Lookups;
+import org.openide.util.lookup.ProxyLookup;
+import org.puzzle.puzzlecore.project.GISProject;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class GISMapNode extends FilterNode{
+    
+    private static final String ICON_PATH = "org/puzzle/puzzlecore/project/map.png";
+    private static final Image ICON = Utilities.loadImage(ICON_PATH, true);
+    
+    private final GISProject project;
+    
+    public GISMapNode(Node node, GISProject project) throws DataObjectNotFoundException{
+        super (node, new FilterNode.Children (node),
+                    //The projects system wants the project in the Node's lookup.
+                    //NewAction and friends want the original Node's lookup.
+                    //Make a merge of both
+                    new ProxyLookup (new Lookup[] { Lookups.singleton(project),
+                    node.getLookup() }));
+            this.project = project;
+    }
+
+    @Override
+    public Image getIcon(int type) {
+        return ICON;
+    }
+
+    @Override
+    public Image getOpenedIcon(int type) {
+        return getIcon(type);
+    }
+    
+    @Override
+    public String getDisplayName() {
+        return "Maps";
+    }
+    
+}
