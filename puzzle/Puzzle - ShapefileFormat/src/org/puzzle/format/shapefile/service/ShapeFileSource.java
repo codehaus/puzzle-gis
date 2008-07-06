776519140cff14a05724177c41bf9a90e10c8088
eclesia <eclesia@80afd94b-f245-0410-a355-d48bdef141ff>
2008-07-06 17:04:56 +0000

    update shapefile service and source.

--- puzzle/Puzzle - ShapefileFormat/src/org/puzzle/format/shapefile/service/ShapeFileSource.java
+++ puzzle/Puzzle - ShapefileFormat/src/org/puzzle/format/shapefile/service/ShapeFileSource.java
@@ -5,9 +5,13 @@
 
 package org.puzzle.format.shapefile.service;
 
+import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
 import java.util.Map;
+import javax.swing.ImageIcon;
 import org.apache.commons.collections.map.SingletonMap;
 import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFinder;
@@ -15,6 +19,7 @@ import org.geotools.data.FeatureSource;
 import org.geotools.gui.swing.misc.Render.RandomStyleFactory;
 import org.geotools.styling.Style;
 import org.openide.util.Exceptions;
+import org.openide.util.Utilities;
 import org.puzzle.puzzlecore.context.LayerSource;
 import org.puzzle.puzzlecore.context.RichMapLayer;
 import org.puzzle.puzzlecore.project.source.GISSource;
@@ -25,14 +30,21 @@ import org.puzzle.puzzlecore.project.source.GISSource;
  */
 public class ShapeFileSource implements GISSource{
 
+    
+    private static final String IMAGE_ICON_BASE = "org/puzzle/format/shapefile/shapefile.png";
+    
     private final int id;
+    private final Map<String,String> parameters;
     private final String name;
+    private final String serviceName;
     private FeatureSource featureSource = null;
     
     
-    ShapeFileSource(File shapefile, int id){
+    ShapeFileSource(File shapefile, String serviceName,int id, Map<String,String> parameters){
         this.id = id;
         this.name = shapefile.getName();
+        this.serviceName = serviceName;
+        this.parameters = parameters;
         DataStore store = null;
         try {
             store = DataStoreFinder.getDataStore(new SingletonMap("url", shapefile.toURI().toURL()));
@@ -59,6 +71,22 @@ public class ShapeFileSource implements GISSource{
 
     public int getID() {
         return id;
+    }
+
+    public Image getIcon(int type) {
+        return Utilities.loadImage(IMAGE_ICON_BASE);
+    }
+
+    public Map<String, String> getParameters() {
+        return Collections.unmodifiableMap(parameters);
+    }
+
+    public String getTitle() {
+        return name;
+    }
+
+    public String getServiceName() {
+        return serviceName;
     }
 
 }
