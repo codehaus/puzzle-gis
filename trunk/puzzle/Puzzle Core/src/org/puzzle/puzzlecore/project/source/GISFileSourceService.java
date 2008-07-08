4988b1da1480dbbc5b1e122f3316c36d3cf27e14
anaxa_gore <anaxa_gore@80afd94b-f245-0410-a355-d48bdef141ff>
2008-07-08 13:44:53 +0000

    Complete javadoc for package org.puzzle.puzzlecore.project.source

--- trunk/puzzle/Puzzle Core/src/org/puzzle/puzzlecore/project/source/GISFileSourceService.java
+++ trunk/puzzle/Puzzle Core/src/org/puzzle/puzzlecore/project/source/GISFileSourceService.java
@@ -1,21 +1,49 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  Puzzle-GIS - OpenSource mapping program
+ *  http://docs.codehaus.org/display/PUZZLEGIS
+ *  Copyright (C) 2007 Puzzle-GIS
+ *  
+ *  GPLv3 + Classpath exception
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.puzzle.puzzlecore.project.source;
 
 import java.io.File;
 import javax.swing.filechooser.FileFilter;
 
 /**
+ * This class is a special {@code GISSourceService} dedicated to file datas.<br>
+ * Indeed, files get a number of particular features such as "extension" etc, 
+ * which should be handled in a special way.
  * 
- * @author Administrateur
+ * @author  Johann Sorel
+ * @author  Thomas Bonavia (comments)
  */
 public interface GISFileSourceService extends GISSourceService{
-
+    /**
+     * Creates a {@code FileFilter} corresponding to the data file to handle.
+     * @return  A new {@code FileFilter}.
+     */
     FileFilter createFilter();
     
+    /**
+     * Creates the {@code GISSource} associated with the data {@code file}.
+     * @param   file  The data to represent throw the {@code GISSource}.
+     * @return  A new {@code GISSource} associated with the {@code file}.
+     * @throws  java.lang.IllegalArgumentException
+     */
     GISSource createSource(File file) throws IllegalArgumentException;
     
 }
--- trunk/puzzle/Puzzle Core/src/org/puzzle/puzzlecore/project/source/GISSourceService.java
+++ trunk/puzzle/Puzzle Core/src/org/puzzle/puzzlecore/project/source/GISSourceService.java
@@ -1,20 +1,49 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  Puzzle-GIS - OpenSource mapping program
+ *  http://docs.codehaus.org/display/PUZZLEGIS
+ *  Copyright (C) 2007 Puzzle-GIS
+ *  
+ *  GPLv3 + Classpath exception
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.puzzle.puzzlecore.project.source;
 
 import java.util.Map;
 
 /**
- *
- * @author Johann Sorel
+ * This class describe a generic way of managing {@code GISSource}s.
+ * 
+ * @author  Johann Sorel
+ * @author  Thomas Bonavia (comments)
  */
 public interface GISSourceService {
-
+    
+    /**
+     * Get the service's identifier.
+     * @return A {@code String} containing the id.
+     */
     String getIdentifier();
     
-    GISSource restoreSource(Map<String,String> parameters,int id) throws IllegalArgumentException;
+    /**
+     * Rebuild the {@code GISSource} from the parameters of the XML file.
+     * @param   parameters  The parameters of the XML file.
+     * @param   id          The source ID.
+     * @return  The rebuilt {@code GISSource}.
+     * @throws  java.lang.IllegalArgumentException
+     */
+    GISSource restoreSource(Map<String,String> parameters,int id)
+            throws IllegalArgumentException;
     
 }
