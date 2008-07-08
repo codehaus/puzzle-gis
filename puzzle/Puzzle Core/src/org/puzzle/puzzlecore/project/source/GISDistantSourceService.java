526462ae3fd39c89a9da91e380fb3158cd85828c
anaxa_gore <anaxa_gore@80afd94b-f245-0410-a355-d48bdef141ff>
2008-07-08 13:44:53 +0000

    Complete javadoc for package org.puzzle.puzzlecore.project.source

--- puzzle/Puzzle Core/src/org/puzzle/puzzlecore/project/source/GISDistantSourceService.java
+++ puzzle/Puzzle Core/src/org/puzzle/puzzlecore/project/source/GISDistantSourceService.java
@@ -1,21 +1,61 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  Puzzle-GIS - OpenSource mapping program
+ *  http://docs.codehaus.org/display/PUZZLEGIS
+ *  Copyright (C) 2007 Puzzle-GIS
+ *  
+ *  GPLv3 + Classpath exception
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.puzzle.puzzlecore.project.source;
 
 import java.util.Map;
 import org.geotools.gui.swing.datachooser.DataPanel;
 
 /**
- *
- * @author Administrateur
+ * This class is a special {@code GISSourceService} dedicated to distant datas,
+ * such as tables in a database.
+ * <br>
+ * Indeed, to get access to this kind of datas, it is needed to know some
+ * special parameters. For instance, in case of a database table, we need
+ * <ul>
+ *  <li>The server address,</li>
+ *  <li>The port,</li>
+ *  <li>The username/password,</li>
+ *  <li>The name of the table,</li>
+ *  <li>...</li>
+ * </ul>
+ * 
+ * @author  Johan Sorel
+ * @author  Thomas Bonavia (comments)
  */
 public interface GISDistantSourceService extends GISSourceService{
-
+    /**
+     * Creates a {@code DataPanel} to allow the user entering informations
+     * needed to retrieve distant datas. This {@code DataPanel} is a graphical
+     * component.
+     * @return  A {@code DataPanel}.
+     */
     DataPanel createDataPanel();
     
+    /**
+     * Creates the {@code GISSource} to associated with the distant data
+     * described by the {@code parameters} entered by the user in the 
+     * {@code DataPanel}.
+     * @param   parameters  The parameters describing the distant data.
+     * @return  A new {@code GISSource} associated with the distant data.
+     * @throws  java.lang.IllegalArgumentException
+     */
     GISSource createSource(Map parameters) throws IllegalArgumentException;
-    
 }
