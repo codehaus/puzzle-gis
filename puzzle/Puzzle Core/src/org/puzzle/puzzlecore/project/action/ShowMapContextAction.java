5d8527ca32f72f987d168298870195eeaab45b53
anaxa_gore <anaxa_gore@80afd94b-f245-0410-a355-d48bdef141ff>
2008-07-08 16:17:08 +0000

    Complete javadoc for package org.puzzle.puzzlecore.project.action

--- puzzle/Puzzle Core/src/org/puzzle/puzzlecore/project/action/ShowMapContextAction.java
+++ puzzle/Puzzle Core/src/org/puzzle/puzzlecore/project/action/ShowMapContextAction.java
@@ -1,10 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  Puzzle-GIS - OpenSource mapping program
+ *  http://docs.codehaus.org/display/PUZZLEGIS
+ *  Copyright (C) 2007 Puzzle-GIS
+ *  
+ *  GPLv3 + Classpath exception
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.puzzle.puzzlecore.project.action;
 
-import javax.swing.JOptionPane;
 import org.geotools.map.MapContext;
 import org.openide.nodes.Node;
 import org.openide.util.HelpCtx;
@@ -17,8 +32,21 @@ import org.puzzle.puzzlecore.view.MapView;
 import org.puzzle.puzzlecore.view.RenderingService;
 import org.puzzle.puzzlecore.view.ViewService;
 
+/**
+ * An action allowing the user to display a context in a map.
+ * 
+ * @author  Johann Sorel
+ * @author  Thomas Bonavia (comments)
+ * 
+ * @see     org.openide.util.actions.CookieAction
+ */
 public final class ShowMapContextAction extends CookieAction {
 
+    /**
+     * Method called when this action is clicked.<br>
+     * This method displays the first activated context node in a map.
+     * @param   activatedNodes  The currently activated nodes.
+     */
     protected void performAction(Node[] activatedNodes) {
         GISContextDataObject dataObject = activatedNodes[0].getLookup().lookup(GISContextDataObject.class);
     
@@ -31,23 +59,30 @@ public final class ShowMapContextAction extends CookieAction {
         
         ViewService viewService = Lookup.getDefault().lookup(ViewService.class);
         viewService.add(view);
-        
-        
-//        JOptionPane.showMessageDialog(null, "hahaha");
-        
     }
 
-    
-    
-    
+    /**
+     * Specify the enable mode of the action.<br>
+     * This one is enable when only one node is selected.
+     * @return  An {@code int} containing the mode.
+     */
     protected int mode() {
         return CookieAction.MODE_EXACTLY_ONE;
     }
 
+    /**
+     * Get the name of the action.
+     * @return  A {@code String} containing the Action's name.
+     */
     public String getName() {
         return NbBundle.getMessage(ShowMapContextAction.class, "CTL_ShowMapContextAction");
     }
 
+    /**
+     * Defines the cookies needed for the action to be enable. The activated
+     * node must contain a {@code GISContextDataObject} to be enabled.
+     * @return  A set of {@code Class}es.
+     */
     protected Class[] cookieClasses() {
         return new Class[]{GISContextDataObject.class};
     }
