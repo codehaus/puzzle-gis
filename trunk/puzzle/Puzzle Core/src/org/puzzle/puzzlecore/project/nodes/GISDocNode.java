429380d0bdc39ed669f10d683f5078bb25981c80
anaxa_gore <anaxa_gore@80afd94b-f245-0410-a355-d48bdef141ff>
2008-07-08 14:40:47 +0000

    Complete javadoc for package org.puzzle.puzzlecore.project.nodes

--- trunk/puzzle/Puzzle Core/src/org/puzzle/puzzlecore/project/nodes/GISDocNode.java
+++ trunk/puzzle/Puzzle Core/src/org/puzzle/puzzlecore/project/nodes/GISDocNode.java
@@ -1,8 +1,23 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  Puzzle-GIS - OpenSource mapping program
+ *  http://docs.codehaus.org/display/PUZZLEGIS
+ *  Copyright (C) 2007 Puzzle-GIS
+ *  
+ *  GPLv3 + Classpath exception
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.puzzle.puzzlecore.project.nodes;
 
 import java.awt.Image;
@@ -16,17 +31,29 @@ import org.openide.util.lookup.ProxyLookup;
 import org.puzzle.puzzlecore.project.GISProject;
 
 /**
- *
- * @author Administrateur
+ * This class provides a {@link org.openide.nodes.Node} for the "doc" folder
+ * defined in the {@link org.puzzle.puzzlecore.project.GISProject}.
+ * This folder is intended to contain all files document files, used to make
+ * reports or printable maps...
+ * 
+ * @author  Johann Sorel
+ * @author  Thomas Bonavia (comments)
+ * 
+ * @see     org.openide.nodes.FilterNode
  */
 public class GISDocNode extends FilterNode{
     
     private static final String ICON_PATH = "org/puzzle/puzzlecore/project/doc.png";
-    
     private static final Image ICON = Utilities.loadImage(ICON_PATH, true);
     
     private final GISProject project;
     
+    /**
+     * Constructor.
+     * @param   node      The {@code Node} describing the folder.
+     * @param   project   The current {@code GISProject}.
+     * @throws  org.openide.loaders.DataObjectNotFoundException
+     */
     public GISDocNode(Node node, GISProject project) throws DataObjectNotFoundException{
         super (node, new FilterNode.Children (node),
                     //The projects system wants the project in the Node's lookup.
