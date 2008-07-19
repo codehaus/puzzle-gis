9b27ead43fa9482f9f20921b96f6c8b731bddcef
eclesia <eclesia@80afd94b-f245-0410-a355-d48bdef141ff>
2008-07-19 16:29:26 +0000

    Added postgis module.

---
+++ trunk/puzzle/Puzzle - PostGISFormat/src/org/puzzle/format/postgis/service/PostGISSource.java
@@ -0,0 +1,93 @@
+/*
+ *  Puzzle-GIS - OpenSource mapping program
+ *  http://docs.codehaus.org/display/PUZZLEGIS
+ *  Copyright (C) 2007-2008 Puzzle-GIS
+ *  
+ *  GPLv3 + Classpath exception
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.puzzle.format.postgis.service;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import org.apache.commons.collections.map.SingletonMap;
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureSource;
+import org.geotools.gui.swing.misc.Render.RandomStyleFactory;
+import org.geotools.styling.Style;
+import org.openide.util.Exceptions;
+import org.openide.util.Utilities;
+import org.puzzle.puzzlecore.context.LayerSource;
+import org.puzzle.puzzlecore.context.RichMapLayer;
+import org.puzzle.puzzlecore.project.source.GISSource;
+
+/**
+ *
+ * @author  Johann Sorel
+ */
+public class PostGISSource implements GISSource{
+
+    private static final String IMAGE_ICON_BASE = "org/puzzle/format/postgis/postgres.png";
+    
+    private final int id;
+    private final Map<String,String> parameters;
+    private final String name;
+    private final String serviceName;
+    private FeatureSource featureSource = null;
+    
+    
+    PostGISSource(String serviceName,int id, Map<String,String> parameters){
+        this.id = id;
+        this.name = "hahaha";
+        this.serviceName = serviceName;
+        this.parameters = parameters;
+                
+    }
+    
+    public RichMapLayer createLayer(Map<String, String> parameters) {
+        Style style = new RandomStyleFactory().createRandomVectorStyle(featureSource);
+        LayerSource source = new LayerSource(id, parameters);
+        RichMapLayer layer = new RichMapLayer(featureSource, style,source);
+        layer.setTitle(name);
+        
+        return layer;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public Image getIcon(int type) {
+        return Utilities.loadImage(IMAGE_ICON_BASE);
+    }
+
+    public Map<String, String> getParameters() {
+        return Collections.unmodifiableMap(parameters);
+    }
+
+    public String getTitle() {
+        return name;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+}
---
+++ trunk/puzzle/Puzzle - PostGISFormat/src/org/puzzle/format/postgis/service/PostGISSourceService.java
@@ -0,0 +1,63 @@
+/*
+ *  Puzzle-GIS - OpenSource mapping program
+ *  http://docs.codehaus.org/display/PUZZLEGIS
+ *  Copyright (C) 2007-2008 Puzzle-GIS
+ *  
+ *  GPLv3 + Classpath exception
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.puzzle.format.postgis.service;
+
+import java.io.File;
+import java.util.Map;
+import org.geotools.gui.swing.datachooser.DataPanel;
+import org.geotools.gui.swing.datachooser.JPostGISDataPanel;
+import org.puzzle.puzzlecore.project.source.GISDistantSourceService;
+import org.puzzle.puzzlecore.project.source.GISSource;
+
+/**
+ *
+ * @author  Johann Sorel
+ */
+public class PostGISSourceService implements GISDistantSourceService{
+
+    private static final String TITLE = "PostGIS";
+    
+    public String getIdentifier(){
+        return "PostGIS";
+    }
+    
+    public GISSource restoreSource(Map<String, String> parameters, int id) throws IllegalArgumentException{
+        final String url = parameters.get("url");
+        
+        if(url == null) throw new IllegalArgumentException("missing parameter url");
+        
+        File shapeFile = new File(url);
+        GISSource postgisSource = new PostGISSource(getIdentifier(),id,parameters);
+        return postgisSource;
+    }
+
+    public DataPanel createDataPanel() {
+        return new JPostGISDataPanel();
+    }
+
+    public GISSource createSource(Map parameters) throws IllegalArgumentException {
+        return null;
+    }
+
+    public String getTitle() {
+        return TITLE;
+    }
+}
