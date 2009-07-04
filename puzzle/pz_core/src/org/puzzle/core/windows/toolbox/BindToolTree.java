2eb28fffee05ab43637d0ba4a92f5cf28106d5bf
eclesia <eclesia@80afd94b-f245-0410-a355-d48bdef141ff>
2009-07-04 19:25:53 +0000

    Added tool to create mosaic

--- puzzle/pz_core/src/org/puzzle/core/windows/toolbox/BindToolTree.java
+++ puzzle/pz_core/src/org/puzzle/core/windows/toolbox/BindToolTree.java
@@ -1,25 +1,22 @@
 /*
- *  Puzzle-GIS - OpenSource mapping program
- *  http://docs.codehaus.org/display/PUZZLEGIS
- *  Copyright (C) 2007-2008 Puzzle-GIS
- *  
- *  GPLv3 + Classpath exception
- *  
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *    Puzzle GIS - Desktop GIS Platform
+ *    http://puzzle-gis.codehaus.org
+ *
+ *    (C) 2007-2009, Johann Sorel
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 3 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
 package org.puzzle.core.windows.toolbox;
 
+import java.awt.Component;
 import java.util.Collection;
 
 import org.openide.DialogDescriptor;
@@ -47,14 +44,24 @@ public class BindToolTree extends Tree implements LookupListener {
         addToolTreeListener(new ToolTreeListener() {
 
             @Override
-            public void treeToolActivated(ToolDescriptor tool) {
-                
-                final Object close = Utilities.getString("close");
-                final Object[] objs = new Object[]{close};
-                
-                final DialogDescriptor desc = new DialogDescriptor(tool.getComponent(), tool.getTitle(), false, objs, close,DialogDescriptor.DEFAULT_ALIGN,null, null);
-                desc.setClosingOptions(objs);
-                DialogDisplayer.getDefault().notify(desc);
+            public void treeToolActivated(final ToolDescriptor tool) {
+
+                new Thread(){
+
+                    @Override
+                    public void run() {
+                        final Object close = Utilities.getString("close");
+                        final Object[] objs = new Object[]{close};
+
+                        Component comp = tool.getComponent();
+                        if(comp != null){
+                            final DialogDescriptor desc = new DialogDescriptor(tool.getComponent(), tool.getTitle(), false, objs, close,DialogDescriptor.DEFAULT_ALIGN,null, null);
+                            desc.setClosingOptions(objs);
+                            DialogDisplayer.getDefault().notify(desc);
+                        }
+                    }
+
+                }.start();
                 
             }
         });
