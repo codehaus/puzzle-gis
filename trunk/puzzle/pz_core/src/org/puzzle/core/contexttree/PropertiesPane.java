2d833815297621d116fbf15b1cd52f3a6b1b671a
eclesia <eclesia@80afd94b-f245-0410-a355-d48bdef141ff>
2009-07-04 15:12:56 +0000

    Change license to LGPL

--- trunk/puzzle/pz_core/src/org/puzzle/core/contexttree/PropertiesPane.java
+++ trunk/puzzle/pz_core/src/org/puzzle/core/contexttree/PropertiesPane.java
@@ -1,14 +1,13 @@
 /*
- *    Geotoolkit - An Open Source Java GIS Toolkit
- *    http://www.geotoolkit.org
+ *    Puzzle GIS - Desktop GIS Platform
+ *    http://puzzle-gis.codehaus.org
  *
- *    (C) 2007 - 2008, Open Source Geospatial Foundation (OSGeo)
- *    (C) 2008 - 2009, Johann Sorel
+ *    (C) 2007-2009, Johann Sorel
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
  *    License as published by the Free Software Foundation;
- *    version 2.1 of the License.
+ *    version 3 of the License.
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
