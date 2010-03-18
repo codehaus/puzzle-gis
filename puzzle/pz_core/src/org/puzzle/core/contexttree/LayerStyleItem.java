d6f1a10265360db504db447f6835bea5702188be
eclesia <eclesia@80afd94b-f245-0410-a355-d48bdef141ff>
2010-03-18 09:11:50 +0000

    Context Tree : add layer style edit action

---
+++ puzzle/pz_core/src/org/puzzle/core/contexttree/LayerStyleItem.java
@@ -0,0 +1,29 @@
+/*
+ *    Puzzle GIS - Desktop GIS Platform
+ *    http://puzzle-gis.codehaus.org
+ *
+ *    (C) 2010, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 3 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.puzzle.core.contexttree;
+
+/**
+ * This class is made to extend the menu item from geotoolkit, this
+ * will implicitly make the class part of this module class-loader and
+ * allow it to be loaded using the layer.xml.
+ * 
+ * @author Johann Sorel (Geomatys)
+ */
+public class LayerStyleItem extends org.geotoolkit.gui.swing.contexttree.menu.LayerStyleItem{
+
+}
