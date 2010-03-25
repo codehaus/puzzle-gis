82d6b304c54d0f5e744e82ce4340e41b2a422eb7
eclesia <eclesia@80afd94b-f245-0410-a355-d48bdef141ff>
2010-03-25 19:39:31 +0000

    WWJ : display geometries under 1 degree envelope

--- puzzle/pz_worldwind/src/org/puzzle/renderer/worldwind/WorldWindUtils.java
+++ puzzle/pz_worldwind/src/org/puzzle/renderer/worldwind/WorldWindUtils.java
@@ -17,6 +17,7 @@
 
 package org.puzzle.renderer.worldwind;
 
+import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.MultiLineString;
@@ -24,8 +25,17 @@ import com.vividsolutions.jts.geom.MultiPoint;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.LatLon;
+import gov.nasa.worldwind.geom.Sphere;
+import gov.nasa.worldwind.geom.Vec4;
 import gov.nasa.worldwind.globes.Globe;
 import gov.nasa.worldwind.render.Renderable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import org.geotoolkit.geometry.jts.JTS;
 import org.opengis.geometry.MismatchedDimensionException;
 import org.opengis.referencing.operation.MathTransform;
 import org.opengis.referencing.operation.TransformException;
@@ -39,16 +49,16 @@ public final class WorldWindUtils {
     private WorldWindUtils(){}
 
 
-    public static Renderable toRenderable(Geometry geom, MathTransform trs, Globe globe) throws MismatchedDimensionException, TransformException{
+    public static Collection<? extends Renderable> toRenderable(Geometry geom, MathTransform trs, Globe globe) throws MismatchedDimensionException, TransformException{
 
         if(geom instanceof Point){
 
         }else if(geom instanceof MultiPoint){
 
         }else if(geom instanceof LineString){
-            return toRenderableLine((LineString)geom,trs,globe);
+            return toRenderable((LineString)geom,trs,globe);
         }else if(geom instanceof MultiLineString){
-            return toRenderableLine((MultiLineString)geom,trs,globe);
+            return toRenderable((MultiLineString)geom,trs,globe);
         }else if(geom instanceof Polygon){
             return toRenderable((Polygon)geom,trs,globe);
         }else if(geom instanceof MultiPolygon){
@@ -58,42 +68,76 @@ public final class WorldWindUtils {
         return null;
     }
 
-    public static Renderable toRenderableLine(Geometry line, MathTransform trs, Globe globe) throws MismatchedDimensionException, TransformException{
-        return new LineRenderable(line,trs,globe);
+    public static Collection<? extends Renderable> toRenderable(Point point, MathTransform trs, Globe globe) throws MismatchedDimensionException, TransformException{
+        final Geometry geoEPSGWGS84 = JTS.transform(point, trs);
+        final Coordinate crd = geoEPSGWGS84.getCoordinate();
+        final LatLon ll = new LatLon(Angle.fromDegreesLatitude(crd.x), Angle.fromDegreesLongitude(crd.y));
+
+        Vec4 vec = globe.computePointFromPosition(ll,5);
+        Renderable r = new Sphere(vec, 5);
+        return Collections.singleton(r);
     }
 
-    public static Renderable toRenderable(Polygon polygon, MathTransform trs, Globe globe) throws MismatchedDimensionException, TransformException{
-        return new PolygonRenderable(polygon, trs, globe);
+    public static Collection<? extends Renderable> toRenderable(MultiPoint points, MathTransform trs, Globe globe) throws MismatchedDimensionException, TransformException{
+        final List<Renderable> rds = new ArrayList<Renderable>();
+        for(int i=0,n=points.getNumGeometries();i<n;i++){
+            rds.addAll(toRenderable(points.getGeometryN(i), trs, globe));
+        }
+        return rds;
     }
 
-    public static Renderable toRenderable(MultiPolygon polygon, MathTransform trs, Globe globe) throws MismatchedDimensionException, TransformException{
-        return new PolygonRenderable((Polygon) polygon.getGeometryN(0), trs, globe);
+    public static Collection<? extends Renderable> toRenderable(LineString line, MathTransform trs, Globe globe) throws MismatchedDimensionException, TransformException{
+        return Collections.singleton(new LineRenderable(line,trs,globe));
     }
 
+    public static Collection<? extends Renderable> toRenderable(MultiLineString line, MathTransform trs, Globe globe) throws MismatchedDimensionException, TransformException{
+        final List<Renderable> rds = new ArrayList<Renderable>();
+        for(int i=0,n=line.getNumGeometries();i<n;i++){
+            rds.addAll(toRenderable(line.getGeometryN(i), trs, globe));
+        }
+        return rds;
+    }
 
-//    public static Renderable toRenderable(MultiPolygon polygon, MathTransform trs) throws MismatchedDimensionException, TransformException{
-//        final Geometry geoEPSGWGS84 = JTS.transform(polygon, trs);
-//        final Coordinate[] coords = geoEPSGWGS84.getCoordinates();
-//
-//        List<LatLon> latlons = new ArrayList<LatLon>();
-//        for (int i = 0; i < coords.length; i++) {
-//            latlons.add(new LatLon(Angle.fromDegreesLatitude(coords[i].x), Angle.fromDegreesLongitude(coords[i].y)));
-//        }
-//
-//        gov.nasa.worldwind.render.airspaces.Polygon wwjLine = new gov.nasa.worldwind.render.airspaces.Polygon();
-//        wwjLine.setLocations(latlons);
-//        wwjLine.setTerrainConforming(true);
-//        wwjLine.setAltitudes(5, 30);
-//        wwjLine.setEnableCaps(false);
-//        //wwjLine.setEnableCaps(false);
-//        wwjLine.setEnableLevelOfDetail(false);
-////        wwjLine.setPositions(positions);
-////        wwjLine.setFollowTerrain(true);
-////        wwjLine.setLineWidth(1);
-////        wwjLine.setColor(Color.RED);
-//
-//        return wwjLine;
+//    public static Renderable toRenderable(Polygon polygon, MathTransform trs, Globe globe) throws MismatchedDimensionException, TransformException{
+//        return new PolygonRenderable(polygon, trs, globe);
 //    }
+//
+//    public static Renderable toRenderable(MultiPolygon polygon, MathTransform trs, Globe globe) throws MismatchedDimensionException, TransformException{
+//        return new PolygonRenderable((Polygon) polygon.getGeometryN(0), trs, globe);
+//    }
+
+    public static Collection<? extends Renderable> toRenderable(MultiPolygon polygon, MathTransform trs, Globe globe) throws MismatchedDimensionException, TransformException{
+        final List<Renderable> rds = new ArrayList<Renderable>();
+        for(int i=0,n=polygon.getNumGeometries();i<n;i++){
+            rds.addAll(toRenderable(polygon.getGeometryN(i), trs, globe));
+        }
+        return rds;
+    }
+
+
+    public static Collection<? extends Renderable> toRenderable(Polygon polygon, MathTransform trs, Globe globe) throws MismatchedDimensionException, TransformException{
+        final Geometry geoEPSGWGS84 = JTS.transform(polygon, trs);
+        final Coordinate[] coords = geoEPSGWGS84.getCoordinates();
+
+        List<LatLon> latlons = new ArrayList<LatLon>();
+        for (int i = 0; i < coords.length; i++) {
+            latlons.add(new LatLon(Angle.fromDegreesLatitude(coords[i].x), Angle.fromDegreesLongitude(coords[i].y)));
+        }
+
+        gov.nasa.worldwind.render.airspaces.Polygon wwjLine = new gov.nasa.worldwind.render.airspaces.Polygon();
+        wwjLine.setLocations(latlons);
+        wwjLine.setTerrainConforming(true);
+        wwjLine.setAltitudes(5, 30);
+        wwjLine.setEnableCaps(false);
+        //wwjLine.setEnableCaps(false);
+        wwjLine.setEnableLevelOfDetail(false);
+//        wwjLine.setPositions(positions);
+//        wwjLine.setFollowTerrain(true);
+//        wwjLine.setLineWidth(1);
+//        wwjLine.setColor(Color.RED);
+
+        return Collections.singleton(wwjLine);
+    }
 
 
 
